0fc1caa44f2474258f3ded2e7cc91ed2306cba78
shohag <mhshohag.ice@gmail.com>
2020-08-20 00:29:05 +0600

    first commit

---
+++ myblog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+#from . import views
+from .views import IndexView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+
+urlpatterns = [
+    #path('', views.index, name="index"),
+    path('', IndexView.as_view(), name="index"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article_details"),
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
+
+]
---
+++ myblog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
+from .forms import EditForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+#def index(request):
+	#return render(request, 'myblog/index.html', {})
+
+class IndexView(ListView):
+	model = Post
+	template_name = 'myblog/index.html'
+	#ordering = ['-id']
+
+class ArticleDetailView(DetailView):
+	model = Post
+	template_name = 'myblog/article_details.html'
+
+class AddPostView(CreateView):
+	model = Post
+	form_class = PostForm
+	template_name = 'myblog/add_post.html'
+	#fields = '__all__'
+	#fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+	model = Post
+	form_class = EditForm
+	template_name = 'myblog/update_post.html'
+	#fields = ['title', 'title_tag', 'body']
+
+class DeletePostView(DeleteView):
+	model = Post
+	template_name = 'myblog/delete_post.html'
+	success_url = reverse_lazy('index')
